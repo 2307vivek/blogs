6924974347ec91d1c52f604868f0a3ac1532f9ef
Vivek Singh <2307vivek@gmail.com>
2024-04-29 15:08:59 +0530

    refactored firebase.go

--- backend/internal/auth/firebase.go
+++ backend/internal/auth/firebase.go
@@ -11,7 +11,9 @@ import (
 	firebase "firebase.google.com/go/v4"
 )
 
-func InitFirebase() *auth.Client {
+var AuthClient *auth.Client
+
+func InitFirebase() {
 	serviceAccountKeyPath, err := filepath.Abs("../../service_account_key.json")
 	utils.FailOnError(err, "Failed to load service account key file.")
 
@@ -20,8 +22,8 @@ func InitFirebase() *auth.Client {
 	firebaseApp, err := firebase.NewApp(context.Background(), nil, opt)
 	utils.FailOnError(err, "Unable to initialize a firebase app.")
 
-	authClient, err := firebaseApp.Auth(context.Background())
+	client, err := firebaseApp.Auth(context.Background())
 	utils.FailOnError(err, "Unable to create auth client")
 
-	return authClient
+	 AuthClient = client
 }
