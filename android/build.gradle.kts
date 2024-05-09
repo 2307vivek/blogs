7b614d888ced26a9d7cc7839edee7356efbf65b6
Vivek Singh <2307vivek@gmail.com>
2024-05-09 15:04:07 +0530

    configured spotless

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -7,4 +7,24 @@ buildscript {
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    alias(libs.plugins.spotless) apply false
+}
+
+subprojects {
+    apply(plugin = rootProject.libs.plugins.spotless.get().pluginId)
+
+    configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+        kotlin {
+            target("**/*.kt")
+            targetExclude("${layout.buildDirectory}/**/*.kt")
+            targetExclude("bin/**/*.kt")
+
+            ktlint("0.40.0")
+            licenseHeaderFile(rootProject.file("licenses/APACHE-2.0"), "(package|import|class|interface|object) ")
+        }
+        kotlinGradle {
+            target("**/*.gradle.kts")
+            ktlint()
+        }
+    }
 }
