3ca8ed49663447f497fd1589c3f6699a710e8145
Vivek Singh <2307vivek@gmail.com>
2024-05-10 00:25:52 +0530

    removed prototype code

--- android/app/src/main/java/inn/vivvvek/blogs/MainActivity.kt
+++ android/app/src/main/java/inn/vivvvek/blogs/MainActivity.kt
@@ -16,28 +16,17 @@
 package inn.vivvvek.blogs
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
-import androidx.activity.result.IntentSenderRequest
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.lifecycle.lifecycleScope
+import androidx.hilt.navigation.compose.hiltViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import inn.vivvvek.blogs.auth.GoogleAuthClient
+import inn.vivvvek.blogs.ui.screens.login.LogInScreen
 import inn.vivvvek.blogs.ui.theme.BlogsTheme
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -49,50 +38,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
 
-            val launcher = rememberLauncherForActivityResult(
-                contract = ActivityResultContracts.StartIntentSenderForResult(),
-            ) { result ->
-                if (result.resultCode == RESULT_OK) {
-                    lifecycleScope.launch {
-                        val signInResult =
-                            googleAuthClient.signInWithIntent(result.data ?: return@launch)
-
-                        Log.d("debuggg", signInResult.toString())
-                    }
-                }
-            }
-
             BlogsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Box(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(horizontal = 16.dp),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        LaunchedEffect(Unit) {
-                            Log.d("debuggg issignedin", googleAuthClient.getSignedInUser().toString())
-                        }
-
-                        Button(
-                            onClick = {
-                                lifecycleScope.launch {
-                                    val signInIntentSender = googleAuthClient.signIn()
-                                    launcher.launch(
-                                        IntentSenderRequest
-                                            .Builder(signInIntentSender ?: return@launch)
-                                            .build()
-                                    )
-                                }
-                            }
-                        ) {
-                            Text(text = "Sign in")
-                        }
-                    }
+                    LogInScreen(
+                        viewModel = hiltViewModel(),
+                        navigateToSignUP = {}
+                    )
                 }
             }
         }
