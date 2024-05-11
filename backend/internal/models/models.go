3eb2e2b9c98aa3b81d3156a627823e1200ca02e9
Vivek Singh <2307vivek@gmail.com>
2024-05-11 22:55:03 +0530

    created models file

---
+++ backend/internal/models/models.go
@@ -0,0 +1,18 @@
+package models
+
+import "github.com/mmcdole/gofeed"
+
+type Company struct {
+	Title   string `bson:"title"`
+	HtmlUrl string `bson:"html_url"`
+	XmlUrl  string `bson:"xml_url"`
+}
+
+type Blog struct {
+	Company     Company       `bson:"company"`
+	BlogTitle   string        `bson:"blog_title"`
+	Description string        `bson:"description"`
+	Link        string        `bson:"link"`
+	FeedLink    string        `bson:"feed_link"`
+	Image       *gofeed.Image `bson:"image"`
+}
