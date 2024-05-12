14034cba09fd0c3fa18aef3fb73fcbc835b2cbe6
Vivek Singh <2307vivek@gmail.com>
2024-05-12 18:56:38 +0530

    created article struct

--- backend/internal/models/models.go
+++ backend/internal/models/models.go
@@ -1,11 +1,20 @@
 package models
 
-import "github.com/mmcdole/gofeed"
+import (
+	"github.com/mmcdole/gofeed"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type Company struct {
 	Title   string `bson:"title" json:"title"`
 	HtmlUrl string `bson:"html_url" json:"html_url"`
 	XmlUrl  string `bson:"xml_url" json:"xml_url"`
+}
+
+type Article struct {
+	Id      primitive.ObjectID `bson:"_id" json:"id"`
+	Company Company     `bson:"company" json:"company"`
+	Article gofeed.Item        `bson:"article" json:"article"`
 }
 
 type Blog struct {
