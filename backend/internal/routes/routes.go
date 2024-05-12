13910511621ea036fcb056575645a06a39a14329
Vivek Singh <2307vivek@gmail.com>
2024-05-12 19:42:08 +0530

    added all routes

--- backend/internal/routes/routes.go
+++ backend/internal/routes/routes.go
@@ -1,5 +1,22 @@
 package routes
 
-func Routes() {
-	
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/2307vivek/blogs/internal/routes/handlers"
+	"github.com/gin-gonic/gin"
+)
+
+func Routes(port int) {
+	router := gin.Default()
+
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"msg": "THis is a response."})
+	})
+
+	router.GET("/articles/:company", handlers.FindArticlesByCompany)
+	router.GET("/articles", handlers.GetLatestArticles)
+
+	router.Run(fmt.Sprintf(":%d", port))
 }
