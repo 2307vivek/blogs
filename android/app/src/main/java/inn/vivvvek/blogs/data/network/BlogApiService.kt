f88e82febc8cd44bac50ff69b50f53ed70dbc8dd
Vivek Singh <2307vivek@gmail.com>
2024-05-13 23:33:19 +0530

    created articles endpoint

--- android/app/src/main/java/inn/vivvvek/blogs/data/network/BlogApiService.kt
+++ android/app/src/main/java/inn/vivvvek/blogs/data/network/BlogApiService.kt
@@ -1,6 +1,7 @@
 package inn.vivvvek.blogs.data.network
 
 import inn.vivvvek.blogs.models.Article
+import inn.vivvvek.blogs.models.Articles
 import inn.vivvvek.blogs.models.BlogArticle
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -11,9 +12,11 @@ interface BlogApiService {
     @GET("articles")
     suspend fun getLatestArticles(
         @Query("page") page: Int
-    ): List<BlogArticle>
+    ): Articles
+
+    @GET("articles/{company}")
     suspend fun getArticleByCompany(
         @Query("page") page: Int,
         @Path("company") companyName: String
-    ): List<BlogArticle>
+    ): Articles
 }
