6e9b619427164a7080e638916b96e18141945af6
Vivek Singh <2307vivek@gmail.com>
2024-05-16 16:44:33 +0530

    created ArticleItem

---
+++ android/app/src/main/java/inn/vivvvek/blogs/ui/screens/components/ArticleItem.kt
@@ -0,0 +1,105 @@
+package inn.vivvvek.blogs.ui.screens.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.LocalContentAlpha
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.BrokenImage
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import inn.vivvvek.blogs.R
+import inn.vivvvek.blogs.models.BlogArticle
+import inn.vivvvek.blogs.models.Company
+
+@Composable
+fun ArticleItem(
+    blogArticle: BlogArticle,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Column(modifier = Modifier.weight(1f)) {
+            Text(
+                text = blogArticle.article.title,
+                maxLines = 3,
+                overflow = TextOverflow.Ellipsis,
+                style = MaterialTheme.typography.titleMedium
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            CompanyNamePill(company = blogArticle.company)
+        }
+        if (blogArticle.article.image != null) {
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(blogArticle.article.image.url)
+                    .placeholder(R.drawable.baseline_broken_image_24)
+                    .error(R.drawable.baseline_broken_image_24)
+                    .crossfade(true)
+                    .build(),
+                contentScale = ContentScale.Crop,
+                contentDescription = "Blog image",
+                modifier = Modifier
+                    .requiredWidth(128.dp)
+                    .requiredHeight(72.dp)
+                    .clip(RoundedCornerShape(16.dp))
+            )
+        } else {
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Icon(
+                    imageVector = Icons.Filled.BrokenImage,
+                    contentDescription = "Blog image",
+                    modifier = Modifier
+                        .requiredWidth(128.dp)
+                        .requiredHeight(72.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun CompanyNamePill(
+    company: Company,
+    onClick: (Company) -> Unit = {},
+    modifier: Modifier = Modifier
+) {
+    Surface(
+        color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.1f),
+        contentColor = MaterialTheme.colorScheme.onBackground,
+        shape = RoundedCornerShape(50),
+        modifier = modifier.clickable {
+            onClick(company)
+        },
+    ) {
+        Text(
+            text = company.title,
+            style = MaterialTheme.typography.labelSmall,
+            modifier = Modifier.padding(horizontal = 10.dp, vertical = 6.dp)
+        )
+    }
+}
