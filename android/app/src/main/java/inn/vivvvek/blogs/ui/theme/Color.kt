36b5defcdb071ab478d70e3727d62eff528a7524
Vivek Singh <2307vivek@gmail.com>
2024-05-16 14:26:30 +0530

    updated color scheme

--- android/app/src/main/java/inn/vivvvek/blogs/ui/theme/Color.kt
+++ android/app/src/main/java/inn/vivvvek/blogs/ui/theme/Color.kt
@@ -17,10 +17,16 @@ package inn.vivvvek.blogs.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Yellow800 = Color(0xFFF29F05)
+val colorTextHeading = Color(0xFF163262)
+val colorTextBody = Color(0xFF4E4B66)
+val colorTextBodyLight = Color(0xFFB0B0C3)
+val colorBackground = Color(0xFFF7F7F7)
+val secondary = Color(0xFFFFB19D)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Dark Colors
+val colorDarkText = Color(0xFFF7F7F7)
+val colorPrimaryDark = Color(0xFF002022)
+val colorBackgroundDark = Color(0xFF006064)
+val colorSurfaceDark = Color(0xFF0DFFFF)
+val imageBackgroundTintDark = Color(0xFF006064)
