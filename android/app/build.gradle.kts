ea585525e823cf3c7b555421941104b4a89a0275
Vivek Singh <2307vivek@gmail.com>
2024-05-16 16:44:16 +0530

    added custom items and material dependency

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -67,6 +67,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.material)
     implementation(libs.androidx.icons.extended)
     implementation(libs.compose.nav)
 
