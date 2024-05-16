c9ba56e0f16248dbec29e17b32dc3784fee9051c
Vivek Singh <2307vivek@gmail.com>
2024-05-16 14:28:20 +0530

    added custom fonts

--- android/app/src/main/java/inn/vivvvek/blogs/ui/theme/Type.kt
+++ android/app/src/main/java/inn/vivvvek/blogs/ui/theme/Type.kt
@@ -17,19 +17,29 @@ package inn.vivvvek.blogs.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import inn.vivvvek.blogs.R
+
+private val WorkSans = FontFamily(
+    Font(R.font.work_sans_regular, FontWeight.Normal),
+    Font(R.font.work_sans_medium, FontWeight.Medium),
+    Font(R.font.work_sans_semibold, FontWeight.SemiBold),
+    Font(R.font.work_sans_bold, FontWeight.Bold),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = WorkSans,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
+    ),
+
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
